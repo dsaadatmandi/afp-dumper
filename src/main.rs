51921f5fa9dbf9345a223faf91bda7e152d017b7
David Milad Saadatmandi <davidmilad@gmail.com>
2025-09-27 17:07:33 +0100

    fixes

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,21 @@ use aho_corasick::AhoCorasick;
 
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
-    // let args: Vec<String> = env::args().collect();
-    // let arg1 = &args[1];
     let arg1: &str = "/Users/milad/code/afp-dumper/data/01_Health_Coverage.afp";
 
-    // opens file in read only - alternatives present for reading entire file
     let f = File::open(arg1)?;
-    // construct new array
-    // let mut buffer = Vec::new();
 
     let breader = std::io::BufReader::new(f);
 
-    // read all of file into array
-    // f.read_to_end(&mut buffer)?;
+    let keywords = structured_field_identifiers::keyword_store()?;
+    let page_start = &keywords["PageStart"];
+    let page_end = &keywords["PageEnd"];
 
     let patterns = &[
         b"\x5A\x00\x00", // Start of Document
         b"\x5A\xFF\xFF", // End of Document
-        b"\x5B\x00\x00", // Start of Page
-        b"\x5B\xFF\xFF", // End of Page
+        page_start.as_slice(), // Begin Page (BPG) - D3A8AF
+        page_end.as_slice(),   // End Page (EPG) - D3A9AF
         b"\xD3\x00\x00", // Begin Structured Field
         b"\xD3\xFF\xFF", // End Structured Field
     ];
@@ -34,7 +30,15 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     for mat in ac.stream_find_iter(breader) {
         let mat = mat?;
-        println!("{:?}", mat);
+        match mat.pattern().as_usize() {
+            0 => println!("Start of Document at {}", mat.start()),
+            1 => println!("End of Document at {}", mat.start()),
+            2 => println!("Begin Page (BPG) at {}", mat.start()),
+            3 => println!("End Page (EPG) at {}", mat.start()),
+            4 => println!("Begin Structured Field at {}", mat.start()),
+            5 => println!("End Structured Field at {}", mat.start()),
+            _ => unreachable!(),
+        }
     }
 
     Ok(())
